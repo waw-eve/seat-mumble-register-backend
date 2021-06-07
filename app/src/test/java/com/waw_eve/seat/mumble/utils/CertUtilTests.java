7bff8c536abc63370491708846ee50c661cbf582
KagurazakaNyaa <i@kagurazakanyaa.com>
2021-06-07 11:07:35 +0800

    测试代码覆盖率改善

--- app/src/test/java/com/waw_eve/seat/mumble/utils/CertUtilTests.java
+++ app/src/test/java/com/waw_eve/seat/mumble/utils/CertUtilTests.java
@@ -7,7 +7,6 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
-
 import com.waw_eve.seat.mumble.Configuration;
 
 import org.junit.Test;
@@ -17,8 +16,9 @@ public class CertUtilTests {
     @Test
     public void certUtilTest() throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
         Configuration configuration = new Configuration();
-        configuration.setCaFilePath("/tmp/ca.p12");
+        configuration.setCaFilePath("/tmp/" + String.valueOf(System.currentTimeMillis()) + ".p12");
         configuration.setCaPassword("");
+        CertUtil.init(configuration);
         CertUtil.init(configuration);
         String cn = "Higgs-Ielenia Amastica";
         KeyStore ks = CertUtil.signCert(cn, "ieleniaamastica@waw-eve.com", "War and Wormhole", "");
